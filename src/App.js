59be2336c957e7f925b7de76794113cf57a8796f
Marco Ferreira <marcoavfcc@gmail.com>
2021-04-26 10:28:26 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
+import Frase from './components/Frase';
 
 const Contenedor = styled.div`
   display: flex;
@@ -17,15 +18,34 @@ const Botton = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
-`
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  } 
+
+`;
 
 function App() {
-  const consultarAPI = () => {
-    console.log('Consultando...'); 
+  // state de frases
+  const [frase, setFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const resultado = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await resultado.json();
+    setFrase(frase[0])
   }
+
+  useEffect(() => {
+    consultarAPI()
+  },[])
 
   return (
       <Contenedor>
+        <Frase 
+          frase={frase}
+        />
         <Botton
           onClick={consultarAPI}
         >
